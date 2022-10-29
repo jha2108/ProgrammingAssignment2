fb36f6d4edfd47e45778945cef67205ca981861f
jha2108 <115239708+jha2108@users.noreply.github.com>
2022-10-29 11:51:31 +0530

    Add files via upload

---
+++ Assignment2.R
@@ -0,0 +1,30 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat,...)
+  x$setInverse(m)
+  m
+}
+my_matrix<-makeCacheMatrix(matrix(1:4,2,2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
